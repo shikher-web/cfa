50ee477b179c2800bb8fbcac4bec34e611ac00cf
shikher-web <shikher0628@gmail.com>
2025-10-29 04:41:57 +0530

    feat: Update Indian Rupee icon
    Refactors the SVG path for the Indian Rupee icon to use a more standard and potentially cleaner representation.

--- components/icons.tsx
+++ components/icons.tsx
@@ -35,7 +35,7 @@ export const GlobeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
 
 export const IndianRupeeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-        <path d="M15 8a4.5 4.5 0 0 0-9 0h9zM6 8a4.5 4.5 0 0 1 9 0H6zM6 8H3m3 5h12m-6 5V3"/>
+      <path d="M6 8h12M6 13h12M8 21V3M8 3h5a5 5 0 0 1 0 10H8" />
     </svg>
 );
 
